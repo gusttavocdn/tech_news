66af9ee5f8c0bed639eec9e6692c8bcc906117a9
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-01-22 00:35:55 -0300

    feat: implement scrape_updates function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import time
 import requests
 from requests.exceptions import ReadTimeout, MissingSchema
+from bs4 import BeautifulSoup
 
 
 # Requisito 1
@@ -20,6 +21,12 @@ def fetch(url: str) -> (str | None):
 # Requisito 2
 def scrape_updates(html_content):
     """Seu código deve vir aqui"""
+    soup = BeautifulSoup(html_content, "html.parser")
+    links = [
+        anchor["href"]
+        for anchor in soup.find_all("a", {"class": "cs-overlay-link"})
+    ]
+    return links
 
 
 # Requisito 3

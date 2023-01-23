c13cade7b4053c10f6c21bcd721e7c9ceeb5e886
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-01-22 23:52:23 -0300

    fix: fix dumb trybe test

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,14 +1,47 @@
 import sys
 
 from tech_news.analyzer.ratings import top_5_categories, top_5_news
-from tech_news.menu_helpers import (
-    menu_add_news_to_db,
-    menu_exit,
-    menu_search_by_category,
-    menu_search_by_date,
-    menu_search_by_tag,
-    menu_search_by_title,
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.search_engine import (
+    search_by_category,
+    search_by_date,
+    search_by_tag,
+    search_by_title,
 )
+
+
+def menu_add_news_to_db():
+    try:
+        amount = int(input("Digite quantas notícias serão buscadas: "))
+    except ValueError:
+        print("Quantidade inválida")
+        return
+    else:
+        return get_tech_news(amount)
+
+
+def menu_search_by_title():
+    title = input("Digite o título:")
+    return search_by_title(title)
+
+
+def menu_search_by_date():
+    date = input("Digite a data no formato aaaa-mm-dd:")
+    return search_by_date(date)
+
+
+def menu_search_by_category():
+    category = input("Digite a categoria:")
+    return search_by_category(category)
+
+
+def menu_search_by_tag():
+    tag = input("Digite a tag:")
+    return search_by_tag(tag)
+
+
+def menu_exit():
+    print("Encerrando script")
 
 
 # Requisito 12
